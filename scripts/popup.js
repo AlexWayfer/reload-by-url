837ab16db02b58572850b86caeb8edbf8770cea0
Alexander Popov <alex.wayfer@gmail.com>
2022-12-08 16:43:26 +0300

    Save and read with storage

--- scripts/popup.js
+++ scripts/popup.js
@@ -2,6 +2,83 @@ const [currentTab] = await chrome.tabs.query({
 	active: true, lastFocusedWindow: true
 })
 
-const newSection = document.querySelector('section.new')
+const
+	formNew = document.querySelector('form.new'),
+	newUrlInput = formNew.querySelector('input.url'),
+	addButton = formNew.querySelector('button.add'),
+	updateButton = formNew.querySelector('button.update'),
+	sectionAdded = document.querySelector('section.added'),
+	listAdded = sectionAdded.querySelector('ul.added'),
+	addedItemTemplate = sectionAdded.querySelector('template.added-item'),
+	emptyListNotice = sectionAdded.querySelector('p.empty-list')
 
-newSection.querySelector('input.url').value = currentTab.url
+const getAdded = async () => {
+	const found = await chrome.storage.sync.get({ added: {} })
+	// `.get` returns an object of found results with keys,
+	// not a value of requested by one key
+	return found.added
+}
+
+const refreshListAdded = data => {
+	const dataEntries = Object.entries(data)
+
+	if (dataEntries.length == 0) {
+		listAdded.replaceChildren()
+		listAdded.classList.add('hidden')
+		emptyListNotice.classList.remove('hidden')
+	} else {
+		const newItems =
+			dataEntries.map(([url, props]) => {
+				const newItem = addedItemTemplate.content.cloneNode(true)
+				newItem.querySelector('.url').textContent = url
+				return newItem
+			})
+
+		listAdded.replaceChildren(...newItems)
+
+		emptyListNotice.classList.add('hidden')
+		listAdded.classList.remove('hidden')
+	}
+
+	toggleNewButtons()
+}
+
+const toggleNewButtons = async () => {
+	const
+		added = await getAdded(),
+		isValueExists = added.hasOwnProperty(newUrlInput.value)
+
+	addButton.classList.toggle('hidden', isValueExists)
+	updateButton.classList.toggle('hidden', !isValueExists)
+}
+
+// Initialize "Added" list from storage
+
+refreshListAdded(await getAdded())
+
+// Listen for future updates of "Added"
+
+chrome.storage.onChanged.addListener((changes, _area) => {
+	if ('added' in changes) {
+		refreshListAdded(changes.added.newValue || {})
+	}
+})
+
+// "New" form URL input
+
+newUrlInput.addEventListener('input', async event => {
+	toggleNewButtons()
+})
+
+newUrlInput.value = currentTab.url
+
+// "New" form submitting
+
+formNew.addEventListener('submit', async event => {
+	event.preventDefault()
+
+	const added = await getAdded()
+	added[currentTab.url] = {}
+
+	chrome.storage.sync.set({ added })
+})
