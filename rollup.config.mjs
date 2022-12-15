c4578064d8ec5d7546bb6f82a93c2f8ddd191190
Alexander Popov <alex.wayfer@gmail.com>
2022-12-15 17:13:17 +0300

    Add CommonJS plugin for Rollup
    It's not used now, but it can be useful.
    
    `import Something from 'something'` instead of
    `const Something = require('something')`.

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,4 +1,5 @@
 import multiInput from 'rollup-plugin-multi-input'
+import commonjs from '@rollup/plugin-commonjs'
 import resolve from '@rollup/plugin-node-resolve'
 import notify from 'rollup-plugin-notify'
 
@@ -11,6 +12,7 @@ export default {
 	plugins: [
 		// https://github.com/alfredosalzillo/rollup-plugin-multi-input/issues/61
 		multiInput.default({ relative: 'scripts/src/' }),
+		commonjs(),
 		resolve({
 			browser: true,
 			// It doesn't allow to require `punycode`
